a33d0e02dc9e8bc589c29d4fcbfeb6e25ce9d93f
Manuel Grundner <m.grundner@delegate.at>
2020-10-01 12:11:31 +0200

    feat: push to GH

--- src/Xenial.Commentator.Api/Controllers/WeatherForecastController.cs
+++ src/Xenial.Commentator.Api/Controllers/WeatherForecastController.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Appy.GitDb;
+using Appy.GitDb.Local;
+using Appy.GitDb.Core.Interfaces;
+using Appy.GitDb.Core.Model;
+using LibGit2Sharp;
+using System.IO;
 
 namespace Xenial.Commentator.Api.Controllers
 {
@@ -27,16 +33,56 @@ namespace Xenial.Commentator.Api.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IEnumerable<WeatherForecast>> Get()
         {
+            var dir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            var creds = new UsernamePasswordCredentials
+            {
+                Username = "API_KEY",
+                Password = string.Empty
+            }; 
+
+            var repoPath = Repository.Clone(@"https://github.com/biohazard999/TestingDb.git", dir, new CloneOptions
+            {
+                IsBare = true,
+                CredentialsProvider = (_url, _user, _cred) => creds
+            });
+
+            // 1. Instantiate a new instance of the local git database
+            IGitDb db = new LocalGitDb(repoPath);
+
+            // 2. Save an object to the database
+            var myObject = new SomeClass
+            {
+                SomeProperty = $"SomeValue{Guid.NewGuid()}"
+            };
+
+            await db.Save("master", "commit message", new Document<SomeClass> { Key = "key", Value = myObject }, new Author("Manuel Grundner", "m.grundner@delegate.at"));
+
+            // 3. Retrieve the object
+            var theObject = await db.Get<SomeClass>("master", "key");
+
+            using (var repo = new Repository(repoPath))
+            {
+                Remote remote = repo.Network.Remotes["origin"];
+                var options = new PushOptions();
+                options.CredentialsProvider = (_url, _user, _cred) => creds;
+                repo.Network.Push(remote, @"refs/heads/master", options);
+            }
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = theObject.SomeProperty
             })
             .ToArray();
         }
+    }
+
+    public class SomeClass
+    {
+        public string SomeProperty { get; set; }
     }
 }
